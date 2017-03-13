2638856e682b1409a4376fcb66f9bd56d3a24824
Kev1nR <kevinroche@blueyonder.co.uk>
2017-03-13 09:38:54 +0000

    fixing resizing issue where small factor form disables controls

--- app_server/controllers/survival-models.js
+++ app_server/controllers/survival-models.js
@@ -5,9 +5,8 @@ if (process.env.NODE_ENV === 'production') {
   apiOptions.server = "https://actuarial-mathematics-api.herokuapp.com/";
 }
 
-/* GET Gompertz Law */
-module.exports.gompertzLaw = function(req, res, next){
-    res.render('SurvivalModels/gompertz-law', 
+var renderGompertzResults = function(req, res){
+     res.render('SurvivalModels/gompertz-law', 
         {
             title: 'Gompertz Law',
             description: 'Gompertz Law ... $$ S_x(t) = exp \\left\\{ \\frac{-B}{\\log c} c^x (c^t - 1 ) \\right\\} $$',
@@ -15,15 +14,37 @@ module.exports.gompertzLaw = function(req, res, next){
                 {name: 'B', id:'B', value: null, default:0.001, error:''},
                 {name: 'c', id:'c', value: null, default:1.02, error:''},
                 {name: 'x', id:'x', value: null, default:40, error:''}],
-            results:{
-                data:{
-                    age:40, 
-                    SurvivalData:[
-                        {t:1, mortality:1.2345}, 
-                        {t:2, mortality:2.3456}
-                        ]}, 
-                error:''}
+            // results:{
+            //     data:{
+            //         age:40, 
+            //         SurvivalData:[
+            //             {t:1, mortality:1.2345}, 
+            //             {t:2, mortality:2.3456}
+            //             ]}, 
+            //     error:''}
         });
+}
+
+/* GET Gompertz Law */
+module.exports.gompertzLaw = function(req, res, next){
+    renderGompertzResults(req, res);
+    // res.render('SurvivalModels/gompertz-law', 
+    //     {
+    //         title: 'Gompertz Law',
+    //         description: 'Gompertz Law ... $$ S_x(t) = exp \\left\\{ \\frac{-B}{\\log c} c^x (c^t - 1 ) \\right\\} $$',
+    //         queryParms: [
+    //             {name: 'B', id:'B', value: null, default:0.001, error:''},
+    //             {name: 'c', id:'c', value: null, default:1.02, error:''},
+    //             {name: 'x', id:'x', value: null, default:40, error:''}],
+    //         results:{
+    //             data:{
+    //                 age:40, 
+    //                 SurvivalData:[
+    //                     {t:1, mortality:1.2345}, 
+    //                     {t:2, mortality:2.3456}
+    //                     ]}, 
+    //             error:''}
+    //     });
 }
 
 /* GET Gompertz law args */
--- public/angular/widgets.js
+++ public/angular/widgets.js
@@ -1,1 +1,16 @@
 angular.module('widgets', []);
+
+var gompertzResults = function ($scope){
+    $scope.results = {
+                data:{
+                    age:40, 
+                    SurvivalData:[
+                        {t:1, mortality:1.2345}, 
+                        {t:2, mortality:2.3456}
+                        ]}, 
+                error:''};
+};
+
+angular
+    .module('widgets')
+    .controller('gompertzResults', gompertzResults);
